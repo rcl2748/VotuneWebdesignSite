85e2bc6f78dda02c45cc1c3b6cc0ba9d62935ce4
rcl2748 <rcl2748@ya.ru>
2014-12-18 21:51:30 -0600

    Update

--- src/com/votune/webdesign/beans/User.java
+++ src/com/votune/webdesign/beans/User.java
@@ -1,10 +1,24 @@
 package com.votune.webdesign.beans;
 
+import com.votune.webdesign.util.UserClass;
+
 public class User {
 	
 	private String name;
 	private String password;
 	private String email;
+	private UserClass userClass;
+	
+	public User() {
+		
+	}
+	
+	public User(String name, String password, String email, UserClass userClass) {
+		this.name = name;
+		this.password = password;
+		this.email = email;
+		this.userClass = userClass;
+	}
 	
 	public String getName() {
 		return name;
@@ -28,6 +42,14 @@ public class User {
 	
 	public void setEmail(String email) {
 		this.email = email;
+	}
+	
+	public UserClass getUserClass() {
+		return userClass;
+	}
+	
+	public void setUserClass(UserClass userClass) {
+		this.userClass = userClass;
 	}
 	
 }
